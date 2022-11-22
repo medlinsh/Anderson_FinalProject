34074eb70f934fe3efa0622b603631fac0dab702
sethapple <sethapple@macbook-pro-34.wifi.uc.edu>
2022-11-22 08:53:29 -0500

    Adding basic files

---
+++ Anderson_FinalProject/decryptionPackage/decryption.py
@@ -0,0 +1,10 @@
+'''
+#Name: Dhenuka Suthaharan, Noah Draper, Jon Buck, Seth Medlin
+#email: medlinsh@mail.uc.edu
+#Assignment: Final Project
+#Course: IS 4010
+#Semester/Year: Fall 2022
+#Brief Description: This project demonstrates that we can decrypt and create a program to show photos
+#Citations: N/A
+#Anything else thats relevant: N/A
+'''
