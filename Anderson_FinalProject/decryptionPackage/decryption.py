c719603620348113b137d9e03295a2f6432e9b80
sethapple <sethapple@macbook-pro-34.wifi.uc.edu>
2022-11-22 09:05:31 -0500

    Add decryption files

--- Anderson_FinalProject/decryptionPackage/decryption.py
+++ Anderson_FinalProject/decryptionPackage/decryption.py
@@ -9,4 +9,10 @@
 #Anything else thats relevant: N/A
 '''
 
+import json
 
+#load the JSON file
+with open("C:\\Users\\15137\\Documents\\Fall 2022 Classes\\Advanced App Development\\Final Project\\Final Project Fall 2022-1\\EncryptedGroupHints.json") as json_file:
+    data = json.load(json_file)
+   
+print(type(data))
